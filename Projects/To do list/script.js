c6747c3f276d0082d358551c177cf433bd001297
Manpreet055 <singhmanpreet57577@gmail.com>
2025-04-25 23:24:19 +0400

    delete button functionality done
    delete button functionality done , realize that there is nothing that i know , i should give more time on JS projects to  get comfortable with JS syntax.. stuck at edit button functionality .. will do tommorw

--- Projects/To do list/script.js
+++ Projects/To do list/script.js
@@ -7,28 +7,53 @@ allTasks.forEach((task) => {
 });
 
 addTasks.addEventListener("click", () => {
-  if (inputData.value === "") return;
+  let duplicateCheck = allTasks.some((task) => task.includes(inputData.value));
+  if (inputData.value === "" || duplicateCheck) {
+    alert("Invalid Input!");
+    inputData.value = "";
+    return;
+  }
+
   let newDiv = document.createElement("div");
   newDiv.className = "newDiv";
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.className = "checkbox";
-  let newbutton = document.createElement("button");
-  newbutton.textContent = "Delete";
-  newbutton.className = "deleteTask";
+  let deletebutton = document.createElement("button");
+  deletebutton.textContent = "Delete";
+  deletebutton.className = "deleteTask";
+  let editButton = document.createElement("button");
+  editButton.className = "editTask";
+  editButton.textContent = "Edit";
+  let text = document.createElement("span");
+  text.textContent = inputData.value;
+  text.className = "textData";
   newDiv.appendChild(checkbox);
-  newDiv.append(inputData.value);
-  newDiv.appendChild(newbutton);
+  newDiv.appendChild(text);
+  newDiv.appendChild(editButton);
+  newDiv.appendChild(deletebutton);
   tasks.appendChild(newDiv);
   allTasks.push(newDiv.outerHTML);
-
   localStorage.setItem("alltasks", JSON.stringify(allTasks));
+  inputData.value = "";
 });
 
-tasks.addEventListener("click", (e) => {
-  if (e.target.classList.contains("deleteTask")) {
-    let taskDiv = e.target.closest(".newDiv");
-    taskDiv.remove();
+tasks.addEventListener("click", (event) => {
+  if (event.target.matches(".deleteTask")) {
+    let removeTask = event.target.closest(".newDiv");
+    let removeHtml = removeTask.outerHTML;
+    removeTask.remove();
+    allTasks = allTasks.filter((task) => task !== removeHtml);
     localStorage.setItem("alltasks", JSON.stringify(allTasks));
+  } else if (event.target.matches(".editTask")) {
+    let targetedDiv = event.target.closest(".newDiv");
+    let textElement = targetedDiv.children[1];
+    addTasks.textContent = "Edit";
+    inputData.value = textElement.textContent;
+    textElement.textContent = inputData.value;
+    targetedDiv.remove();
+    allTasks = addTasks.filter((task) => task !== targetedDiv);
+    localStorage.setItem("alltasks", JSON.stringify(allTasks));
+    addTasks.textContent = "+ Add";
   }
 });
